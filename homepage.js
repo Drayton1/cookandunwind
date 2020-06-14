635be59ee4b93b2db0ea1729de7fbe759810b5dc
Drayton1 <54001876+Drayton1@users.noreply.github.com>
2020-06-14 18:08:57 -0400

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,26 @@
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+function swapText() {
+    var x = document.getElementById("myDiv");
+ if (x.innerHTML === "Yes...") {
+    x.innerHTML = "No..."; 
+  } 
+ else {
+    x.innerHTML = "Yes...";
+  }};
+  
+function myFunction() {
+  var element = document.getElementById("myDIV");
+   element.classList.toggle("myStyle");
+} var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");function myFunction2() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
